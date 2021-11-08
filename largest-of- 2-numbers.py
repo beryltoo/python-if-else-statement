0480b4db8533ed9fc5694e4e04e28993bb3b9290
Beryl Chebet <berylchebet44@gmail.com>
2021-11-08 22:21:16 +0300

    Add files via upload

---
+++ largest-of- 2-numbers.py
@@ -0,0 +1,9 @@
+number1 = int(input('Enter your number:'))
+number2 = int(input('enter your number: '))
+
+if number1 > number2:
+    larger_number = number1
+else:
+    larger_number = number2
+
+print('the larger number is ', larger_number)
